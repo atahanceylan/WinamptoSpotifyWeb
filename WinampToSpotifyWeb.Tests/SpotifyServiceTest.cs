f2b870f9dd40fd99d9a2d6f18f35ace31c3c4252
Atahan <atahanceylan@gmail.com>
2023-08-07 23:23:00 +0200

    Unit test project added

---
+++ WinampToSpotifyWeb.Tests/SpotifyServiceTest.cs
@@ -0,0 +1,35 @@
+namespace WinampToSpotifyWeb.Tests
+{
+    public class SpotifyServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetAccessTokenWhenValidCredentialsEntered()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ThrowsErrorWhenInvalidCredentialsEntered()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void GetPlaylistInfoWhenValidFolderSelected()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ThrowsErrorWhenInvalidFolderSelected()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
